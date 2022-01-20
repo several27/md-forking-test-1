59f481cdde49c45cb9da3b1f5b1ebe192e303e21
several27@icloud.com <several27@icloud.com>
2022-01-20 23:05:02 +0000

    new workflow

---
+++ code/scala/workflows/new-workflow/code/src/main/scala/graph/Source_0.scala
@@ -0,0 +1,25 @@
+package graph
+
+import io.prophecy.libs._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import config.ConfigStore._
+
+object Source_0 {
+
+  def apply(spark: SparkSession): DataFrame = {
+    Config.fabricName match {
+      case "livy" =>
+        spark.read
+          .format("parquet")
+          .load(
+            "file:/storage/workflowdata/visa/pdg-data/pdg-data/tcmls_edwcs_b2smsdrf_dtl/"
+          )
+      case _ =>
+        throw new Exception("No valid dataset present to read fabric")
+    }
+  }
+
+}
